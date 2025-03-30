d8f1352e11ec73d3fb8e65a945e287987b8b606e
jsn0509 <jsn_@naver.com>
2025-03-31 05:52:09 +0900

    feat: 메인 페이지 섹션 컴포넌트들 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import { MainIntroSection } from '@/feature/main/MainIntroSection';
+import { AppFunctionSection } from '@/feature/main/AppFunctionSection';
+import { AppDownloadSection } from '@/feature/main/AppDownloadSection';
+import { FAQSection } from '@/feature/main/FAQSection';
+
 export default function Home() {
-  return <div className="h-full w-full overflow-y-auto">홈 페이지</div>;
+  return (
+    <div className="w-screen">
+      {/* 메인 */}
+      <MainIntroSection />
+
+      {/* 앱 기능 소개 (가로 스크롤) */}
+      <AppFunctionSection className="mt-48" />
+
+      {/* 앱 다운로드 배너 */}
+      <AppDownloadSection className="mt-32" />
+
+      {/* 자주 묻는 질문 */}
+      <FAQSection className="mt-32" />
+    </div>
+  );
 }
