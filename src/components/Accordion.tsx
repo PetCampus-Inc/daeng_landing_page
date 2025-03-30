8bab2cc613a4dc5a889142599825343e4ad4447b
jsn0509 <jsn_@naver.com>
2025-03-31 04:50:13 +0900

    design: 아코디언 컴포넌트 스타일 변경

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -11,12 +11,12 @@ type AccordionItemProps = {
 
 export const Accordion = ({ title, children, className }: AccordionItemProps) => {
   return (
-    <details className={cn('group', className)}>
-      <summary className="flex flex-1 gap-4 items-center px-4 py-4 text-24 transition-all cursor-pointer select-none hover:text-primary list-none before:content-['Q'] before:text-foreground-muted/50 before:font-semibold">
+    <details className={cn('group border-b border-border', className)}>
+      <summary className="flex flex-1 gap-4 items-center px-4 py-4 text-24 max-md:text-20 cursor-pointer select-none hover:text-primary list-none before:content-['Q'] before:text-foreground-muted/50 before:font-semibold">
         {title}
       </summary>
 
-      <div className="ml-6 mb-4 px-5 py-4 bg-surface rounded-lg border border-border-accent text-20">
+      <div className="ml-6 mb-4 px-5 py-4 text-foreground/80 bg-surface rounded-lg border border-border-accent text-20 max-md:text-16 whitespace-pre-line">
         {children}
       </div>
     </details>
