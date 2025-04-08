1af047df6ac7ef871d31b938fdb8626cb1161a9b
SouthNova <jsn950509@gmail.com>
2025-04-08 20:39:41 +0900

    hotfix: Next15 이미지 최적화 이슈 unoptimized: true 옵션 임시 활성화

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        pathname: '/PetCampus-Inc/**',
-      },
-    ],
+    unoptimized: true,
+    // Next15 에서 외부 링크 이미지 최적화에 이슈 있음
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'raw.githubusercontent.com',
+    //     pathname: '/PetCampus-Inc/**',
+    //   },
+    // ],
   },
 };
 
